d8bf1da2025b5966ac18bea179a20880e60b95bb
dl57934 <dl57934@pukyong.ac.kr>
2019-03-25 22:57:37 +0900

    complete react native complete rn Seminar App

--- dustPage/db.js
+++ dustPage/db.js
@@ -7,12 +7,12 @@ import {
 import React from "react";
 
 export const backgroudColorArrays = [
-  "rgb(32, 196, 78)",
-  "rgb(255, 219, 38)",
-  "rgb(249, 103, 9)",
-  "rgb(216, 38, 47)",
-  "rgb(160, 47, 217)",
-  "rgb(134, 50, 53)"
+  ["#11998e", "#38ef7d"],
+  ["rgb(254, 149, 10)", "rgb(253, 255, 10)"],
+  ["rgb(185, 106, 39)", "rgb(255, 236, 59)"],
+  ["#ED213A", "#93291E"],
+  ["rgb(160, 47, 217)", "rgb(237, 98, 17)"],
+  ["rgb(134, 50, 53)", "#fff"]
 ];
 
 export const LevelImo = [
@@ -24,4 +24,11 @@ export const LevelImo = [
   <MaterialCommunityIcons name="emoticon-devil" size={130} color="white" />
 ];
 
-const Description = {};
+export const Description = [
+  { statusText: "좋음", explanation: "탁한 공기, 마스크 챙기세요!!!!" },
+  { statusText: "보통", explanation: "지금은 외출하셔도 좋습니다." },
+  { statusText: "나쁨", explanation: "탁한 공기, 마스크 챙기세요!!!!" },
+  { statusText: "상당히 나쁨", explanation: "탁한 공기, 마스크 챙기세요!!!!" },
+  { statusText: "매우 나쁨", explanation: "탁한 공기, 마스크 챙기세요!!!!" },
+  { statusText: "상당히 나쁨", explanation: "탁한 공기, 마스크 챙기세요!!!!" }
+];
--- dustPage/dustPage.js
+++ dustPage/dustPage.js
@@ -1,47 +1,72 @@
 import React, { Component, useState, useEffect } from "react";
 import { View, StyleSheet, Text } from "react-native";
-import { backgroudColorArrays, LevelImo } from "./db";
+import { backgroudColorArrays, LevelImo, Description } from "./db";
+import { LinearGradient } from "expo";
 
 const DustPageView = ({ pollution: { aqius }, weather, positionInfo }) => {
-  console.log(aqius, weather, positionInfo);
   const { city, country } = positionInfo;
   const { tp, ts } = weather;
   const time = ts.split("T")[0];
-  const level = getLevel(aqius);
-  console.log("level" + level);
+  let level = getLevel(aqius);
+  const { statusText, explanation } = Description[level];
+
   return (
-    <View style={{ flex: 1, backgroundColor: backgroudColorArrays[level] }}>
+    <LinearGradient
+      colors={[backgroudColorArrays[level][0], backgroudColorArrays[level][1]]}
+      style={styles.container}
+    >
       <View style={styles.upSection}>
+        <Text style={styles.time}>{time}</Text>
         {LevelImo[level]}
         <Text style={styles.temperate}>{tp}°C</Text>
-        <Text style={styles.temperate}>{time}</Text>
+        <Text style={styles.statusText}>{statusText}</Text>
+        <Text style={styles.explanation}>{explanation}</Text>
       </View>
       <View style={styles.downSection}>
-        <Text style={styles.temperate}>현재위치</Text>
-        <Text style={styles.country}>{country}</Text>
         <Text style={styles.city}>{city}</Text>
+        <Text style={styles.country}>{country}</Text>
       </View>
-    </View>
+    </LinearGradient>
   );
 };
 
 export default DustPageView;
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
   upSection: {
-    flex: 1,
+    flex: 2,
     alignItems: "center",
     justifyContent: "center"
   },
   downSection: {
     flex: 1,
     alignItems: "center",
-    justifyContent: "center",
-    backgroundColor: "blue"
+    justifyContent: "center"
   },
-  temperate: { color: "white", fontWeight: "bold", fontSize: 50 },
-  country: { color: "white", fontWeight: "bold", fontSize: 40 },
-  city: { color: "white", fontWeight: "bold", fontSize: 20 }
+  temperate: {
+    color: "white",
+    fontWeight: "bold",
+    fontSize: 30,
+    marginTop: 15
+  },
+  time: { color: "white", fontSize: 30, marginBottom: 20 },
+  country: { color: "white", fontWeight: "bold", fontSize: 25 },
+  city: { color: "white", fontWeight: "bold", fontSize: 30, marginTop: 5 },
+  statusText: {
+    color: "white",
+    marginTop: 15,
+    fontSize: 30,
+    fontWeight: "bold"
+  },
+  explanation: {
+    color: "white",
+    marginTop: 15,
+    fontSize: 22,
+    fontWeight: "bold"
+  }
 });
 
 const getLevel = aqius => (aqius / 50).toFixed(0);
